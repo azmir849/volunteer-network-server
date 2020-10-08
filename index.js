89af521c65590edc51d7ea2fb08a1a3ddf14858a
azmir849 <azmir15-849@diu.edu.bd>
2020-10-08 13:31:04 +0600

    event db and method created

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true,useUnifiedTopology: true });
 client.connect(err => {
   const users = client.db("volunteer-network").collection("registerUser");
+  const events = client.db("volunteer-network").collection("event");
+
   app.post('/register',(req,res) =>{
       const user = req.body;
       console.log(user)
@@ -28,6 +30,15 @@ client.connect(err => {
         console.log(result);
     })
   })
+
+  app.post('/event',(req,res) =>{
+    const event = req.body;
+    console.log(event)
+  events.insertOne(event)
+  .then(result =>{
+      console.log(result);
+  })
+})
 
   app.get('/registerUser',(req,res) =>{
     users.find({})
